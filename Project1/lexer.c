3fada258da9a1eb4e87b06fa1fe8c0f8e47450e4
macarlino <carlino.matthew@knights.ucf.edu>
2016-09-30 21:35:30 -0400

    Init lexer.c

---
+++ Project1/lexer.c
@@ -0,0 +1,26 @@
+// Compiler Builder 1:
+// Matthew Carlino
+// Jacob Solomon
+// Joseph Freeman
+// Alex Arwin
+// Ryan Rossbach
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef enum token
+{
+    nulsym = 1, identsym, numbersym, plussym, minussym,
+    multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+    gtrsym,geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+    periodsym, becomessym, beginsym,endsym, ifsym, thensym,
+    whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+    readsym, elsesym
+}token_type;
+
+int main(int argc, char *argv[]) 
+{
+    
+}
