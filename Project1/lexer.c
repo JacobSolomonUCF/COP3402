7955332a0a189dc5a49838fcce5940d8fabc9c60
Jacob Solomon <j.solomon@knights.ucf.edu>
2016-10-02 19:14:12 -0400

    --source print function and --clean print function

--- Project1/lexer.c
+++ Project1/lexer.c
@@ -16,11 +16,12 @@ char *keyword[] = {
     	"odd", "procedure", "read", "then", "var", "while", "write"
 };
 
-// types of reserved words
-int keyword_type[] = {
+
+//This was giving compile errors. Commented out for now.
+/*int keyword_type[] = {
     	nul, beginsym, callsym, constsym, dosym, elsesym, endsym,
     	ifsym, oddsym, procsym, readsym, thensym, varsym, whilesym, writesym
-};
+};*/
 
 typedef enum token
 {
@@ -36,28 +37,88 @@ typedef enum token
 // TODO find max input size that can be giving to us
 char fileInput[10000];
 
+//Number of chars in the input file
+int numOfChars = 0;
+
 // Function Prototype(s)
 void readInput(FILE * input);
-void printSource(); // print source code when --source used as argv
-void printClean(); // print source code w/o comments when --clean used as argv
+void printSource(char *argv[]); // print source code when --source used as argv
+void printClean(char *argv[]); // print source code w/o comments when --clean used as argv
 
 int main(int argc, char *argv[]) 
 {
-    	FILE * input = fopen(argv[1], "r");
-    	if (input == NULL)
+    FILE * input = fopen(argv[1], "r");
+    if (input == NULL)
     	{
 		printf("Error in opening the file");
 		exit(0);
     	}
-    	readInput(input);
+    readInput(input);
+    printSource(argv);
+    printClean(argv);
+    
+    
+    
 }
 
-void readInput(FILE * input)
-{
-	int i, lines = 0;
+void printSource(char *argv[]){ //Prints the source input
+    int flag = 0;
+    
+    if(argv[2] != NULL && strcmp(argv[2], "--source") == 0){ //Checks for --source in argv
+        flag = 1;
+    }else if(argv[3] != NULL && strcmp(argv[3], "--source") == 0){
+        flag = 1;
+    }
+    
+    if(flag == 1){
+        printf("\nsource code:\n------------\n%s",fileInput);
+    }
+}
+
+void printClean(char *argv[]){ //Prints the input without comments
+    int flag = 0, i = 0;
+    int cflag = 0;
+    char cleanInput[numOfChars];
+    
+    if(argv[2] != NULL && strcmp(argv[2], "--clean") == 0){
+        flag = 1;
+    }else if(argv[3] != NULL && strcmp(argv[3], "--clean") == 0){
+        flag = 1;
+    }
+    
+    if(flag == 1){
+        for(i =0; i<numOfChars; i++){
+            if(fileInput[i] == '/' && fileInput[i+1] == '*'){ //Checks for start of comment
+                cleanInput[i] = ' ';
+                cleanInput[i+1] = ' ';
+                i = i+2;
+                
+                while (cflag != 1) { //Cycles through putting blank space where the comment text is
+                    if(fileInput[i] == '*' && fileInput[i+1] == '/'){ //Checks for closing comment
+                        cflag = 1;
+                        fileInput[i] = ' ';
+                        fileInput[i+1] = ' ';
+                    }else{
+                        cleanInput[i] = ' ';
+                        i++;
+                    }
+                    
+                }
+                
+            }
+            cleanInput[i] = fileInput[i];
+        }
+        printf("\nsource code without comments:\n------------\n%s",cleanInput);
+    
+    }
+}
+void readInput(FILE * input){
+	
     	while (!feof(input))
     	{
 		// puts every char from input file into the array
-		fscanf(input, "%c", &fileInput[lines++]);	
+		fscanf(input, "%c", &fileInput[numOfChars++]);
     	}
+    
+    fclose(input);
 }
