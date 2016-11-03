f574aaf7896a0dda854f4c2ae73b402f25887704
tfreeman91 <tfreeman91@users.noreply.github.com>
2016-11-02 22:45:16 -0400

    Update parser.c

--- Project2/parser.c
+++ Project2/parser.c
@@ -3,190 +3,247 @@
 int main(void)
 {
     // main calls program
-    // program calls block recursively 
+    // program calls block recursively
     program();
     return 0;
 }
 
+// Appendix D
 void program()
 {
-    block();
-    if (tok != periodsym) 
-	error();
     advance();
+    block();
+    if (tok != periodsym)
+        error();
+    // advance();
+    // printf("Tiny PL/0 program is grammatically correct\n");
 }
 
+// Appendix D
 void block()
 {
-    const_declaration();
-    var_declaration();
+
+    if(tok == constsym) {
+        do {
+            advance();
+            if(tok != identsym)     // Error: expected identifer in constant declaration
+                error();
+            advance();
+            if(tok != eqsym)
+                error();            // Expected '=' after 
+            advance();
+            if(tok !+ numbersym)
+                error();
+            advance();
+        } while(tok == commasym)
+
+        if(tok != semicolonsym)
+            error();
+        advance();
+    }
+
+    if(tok == varsym) {
+        do {
+          advance();
+
+          if(tok != identsym)
+                error();            // Expected identifer in variable declaration
+        } while(tok == commasym)
+
+        if(tok != semicolonsym)
+            error();                // Expected ';' at the end of variable declaration
+        advance();
+    }
+
+    while(tok == procsym) {
+        advance();
+
+        if(tok != identsym)
+            error();
+        advance();
+
+        if(tok != semicolonsym)
+            error();
+        advance();
+        block();
+
+        if(tok != semicolonsym)
+            error();
+        advance();
+    }
+
     statement();
+
 }
 
 void const_declaration()
 {
     if (tok != constsym)
-	return;
+        return;
     advance();
-    
+
     if (tok != identsym)
-	error();
+        error();
     advance();
     ident();
-    
+
     if (tok != equalsym) // "="
-	error();
+        error();
     advance();
-    
+
     if (tok != numbersym)
-	error();
+        error();
     advance();
-    
-    while (tok == commasym)
-    {
-	advance();
-	
-	if (tok != identsym)
-	    error();
-	advance();
-	ident();
-	
-	if (tok != equalsym) // "="
-	    error();
-	advance();
-	
-	if (tok != numbersym)
-	    error();
-	advance();
-	number();
+
+    while (tok == commasym) {
+        advance();
+
+        if (tok != identsym)
+            error();
+        advance();
+        ident();
+
+        if (tok != equalsym) // "="
+            error();
+        advance();
+
+        if (tok != numbersym)
+            error();
+        advance();
+        number();
     }
-    
+
     if (tok != semicolonsym)
-	error();
+        error();
     advance();
 }
 
-void var_declaration()
-{
+void var_declaration() {
+
     if (tok != varsym)
-	return;
+        return;
     advance();
-    
+
     if (tok != identsym)
 	error();
     advance();
-    
-    while (tok == commasym)
-    {
-	advance();
-	if (tok != identsym)
-	    error();
-	advance();
+
+    while (tok == commasym) {
+        advance();
+        if (tok != identsym)
+            error();
+        advance();
     }
-    
+
     if (tok != semicolonsym)
-	error();
+        error();
     advance();
 }
 
+// Appendix D
 void statement()
 {
-    if (tok == identsym)
-    {
-	advance();
-	ident();
-	
-	if (tok != becomessym)
-	    error();
-	advance();
-	// need to call advance() in expression
-	expression();	    
+    if (tok == identsym) {
+        advance();
+        if (tok != becomessym)
+            error();
+        advance();
+        // need to call advance() in expression
+        expression();
     }
-    else if (tok == beginsym)
-    {
-	advance();
-	statement();
-	
-	while (tok == commasym)
-	{
-	    advance();
-	    statement();
-	}
-	
-	if (tok != endsym)
-	    error();
-	advance();
+
+    else if(tok == callsym) {
+        advance();
+        if(tok != identsym)
+            error();
+        advance();
     }
-    else if (tok == ifsym)
-    {
-	advance();
-	condition();
-	
-	if (tok != thensym)
-	    error();
-	advance();
-	statement();
+
+    else if (tok == beginsym) {
+        advance();
+        statement();
+
+        while (tok == semicolonsym) {
+            advance();
+            statement();
+        }
+
+        if(tok != endsym)
+            error();
+        advance();
     }
-    else if (tok == whilesym)
-    {
-	advance();
-	condition();
-	
-	if (tok != dosym)
-	    error();
-	advance();
-	statement();
+
+    else if (tok == ifsym) {
+        advance();
+        condition();
+
+        if (tok != thensym)
+            error();
+        advance();
+        statement();
     }
-    else if (tok == readsym)
-    {
-	advance();
-	if (tok != identsym)
-	    error();
-	indent();
+
+    else if (tok == whilesym) {
+        advance();
+        condition();
+
+        if (tok != dosym)
+            error();
+        advance();
+        statement();
     }
-    else if (tok == writesym)
-    {
-	advance();
-	if (tok != identsym)
-	    error();
-	ident();
+
+    else if (tok == readsym) {
+        advance();
+        if (tok != identsym)
+            error();
+        indent();
+    }
+
+    else if (tok == writesym) {
+        advance();
+        if (tok != identsym)
+            error();
+        ident();
     }
 }
 
+// Appendix D
 void condition()
 {
-    if (tok == oddsym)
-    {
-	advance();
-	expression();
+    if (tok == oddsym) {
+        advance();
+        expression();
     }
-    else
-    {
-	expression();
-	rel_op();
-	expression();
+
+    else {
+        expression();
+        if(tok != relation())
+            error();
+        advance();
+        expression();
     }
 }
 
 void rel_op()
 {
     switch(tok)
-	case equalsym: 
+	case equalsym:
 	    advance();
 	    return;
-	case neqsym: 
+	case neqsym:
 	    advance();
 	    return;
-	case lesssym: 
+	case lesssym:
 	    advance();
 	    return;
-	case leqsym: 
+	case leqsym:
 	    advance();
 	    return;
-	case gtrsym: 
+	case gtrsym:
 	    advance();
 	    return;
-	case geqsym: 
+	case geqsym:
 	    advance();
 	    return;
 	default:
@@ -196,66 +253,70 @@ void rel_op()
 // Still have a few functions I need to finish / double check
 // Think the majority of them are pretty set though
 
-// expression ::= [ "+"| "-" ] term { ("+" | "-") term } .
-void expression() {
+// Appendix D .
+void expression()
+{
 
-    if(tok != plussym || tok != minussym) return;
-    advance();
-    term(); // ?
+    if(tok == plussym || tok == minussym)
+        advance();
+    term();
     while(tok == plussym || tok == minussym) {
         advance();
-        if(tok != identsym) error();
-        advance();
+        term();
     }
-    term(); // ?
-    advance();
 }
 
-// term ::= factor { ( "*" | "/" ) factor } .
-void term() {
+// Appendix D
+void term()
+{
     factor();
+
     while(tok == multsym || tok == slashsym) {
-        factor();
         advance();
+        factor();
     }
 }
 
 // factor ::= ident | number | "(" expression ")" .
-void factor() {
+void factor()
+{
+
     if(tok == identsym)
         advance();
-    if(tok == numsym)
+
+    else if(tok == number())
         advance();
-    if(tok == lparensym) {
+
+    else if(tok == lparensym) {
         advance();
         expression();
-        advance();
         if(tok != rparensym)
             error();
     }
 
-    advance();
-
 }
 
 // number ::= digit {digit}.
-void number() {
+void number()
+{
 
     digit();
-    while(false) {
-        digit();
+    while(digit()) {
+        advance();
     }
 
 }
 
 //ident ::= letter {letter | digit}.
-void ident() {
+void ident()
+{
     letter();
 
 }
 
 //digit ::= “0” | “1” | “2” | “3” | “4” | “5” | “6” | “7” | “8” | “9” .
-void digit() {
+void digit()
+{
 
    if(isdigit(tok))
         advance();
@@ -263,10 +324,17 @@ void digit() {
 }
 
 //letter ::= “a” | “b” | ... | “y” | “z” | “A” | “B” | ... | “Y” | “Z”.
-void letter() {
+void letter()
+{
 
     if(isalpha(tok))
         advance();
+
+}
+
+void error() {
+    printf("There has been an error\n");
+    exit(0);
 
 }
 
