da79f47403147de963e17ac882d29096a09049ce
macarlino <carlino.matthew@knights.ucf.edu>
2016-11-07 14:19:50 -0500

    Added symbol table

--- Project2/compile.c
+++ Project2/compile.c
@@ -12,6 +12,19 @@
 #include <ctype.h>
 #include "tokens.h"
 
+#define MAX_SYMBOL_TABLE_SIZE 100
+
+typedef struct symbol
+{
+	int kind;	// const = 1, var = 2, proc = 3
+	char name[12];  // name up to 11 chars
+	int val;	// number (ASCII value)
+	int level;	// L level
+	int addr;	// M address
+}symbol;
+
+symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
+
 // function prototypes
 void program();
 void block();
